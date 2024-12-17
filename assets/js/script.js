afe882a65ac9114093d6fe3abacdb48595b3c48e
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-12-17 18:25:17 +0000

    FEAT: Add post request

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
 
